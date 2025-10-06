033a7b83dc2d0cf202f2d6f48ecd500030909c07
KEPSEU Franck Celestin <kepseufrankc@gmail.com>
2025-10-06 22:26:26 +0200

    fix: add missing auth export in firebase configuration

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeKl0UsGETPBp33PyCowD8DESP1gu3SX0",
@@ -15,6 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
 
 console.log("Firebase initialisé:", app);
 console.log("Firestore initialisé:", db);
